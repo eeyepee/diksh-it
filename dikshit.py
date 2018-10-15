ac67be983e5d3f21e3560832ef70f343d67feebc
Ibrahim Nezar AlMahfooz <ibrahim.nezar@gmail.com>
2018-10-15 10:12:03 +0300

    Optimizing the code

--- dikshit.py
+++ dikshit.py
@@ -8,14 +8,18 @@ while True:
         break
 fh.close()
 
+'''with open('C:\\Users\\ibrahim.nezar\\Desktop\\english-dict2.txt') as fh:
+    file = fh.readlines()
+    dict = set(file)'''
+
 #Input letters
 letters = input("Please enter your letters: ")
-letters_list=[]
-for l in letters:
-    letters_list.append(l)
+letters_set=set(letters)
+#for l in letters:
+#    letters_list.append(l)
 
 mini = 2 #default value
-maks = len(letters_list)
+maks = len(letters_set)
 mini = input("Minimum length of the word (default is 2): ")
 
 
@@ -33,7 +37,7 @@ for words1 in dict:
 for words2 in newdic:
     ok = 1
     for i in words2:
-        if i in letters_list:
+        if i in letters_set:
             ok = ok * 1
         else:
             ok = ok * 0
